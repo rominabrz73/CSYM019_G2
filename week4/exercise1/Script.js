dc8bf6823043dac86a50cc5a0a0064f0593c8ee1
rominabrz73 <78692965+rominabrz73@users.noreply.github.com>
2024-02-23 16:29:33 +0000

    exersice1

---
+++ week4/exercise1/Script.js
@@ -0,0 +1,15 @@
+function print5(){
+    var num1 = 1; 
+    var num2 =2;
+    var num3 =3;
+    var num4 =4;
+    var num5 =5;
+   
+    alert(num1)
+    alert(num2)
+    alert(num3)
+    alert(num4)
+    alert(num5)
+}
+print5();
+print5();
