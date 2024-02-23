b99c9ce7f5d1a6f8402a6378dc3982dc83776e9b
rominabrz73 <78692965+rominabrz73@users.noreply.github.com>
2024-02-23 17:04:52 +0000

    neww

--- week4/exercise3/script.js
+++ week4/exercise3/script.js
@@ -3,6 +3,9 @@ function myFunction(){
    
     var element = document.getElementById('heading');
     element.firstChild.nodeValue ='Heading';
+    var element = document.getElementById('prgraph');
+    element.firstChild.nodeValue ='Hello';
+    
     
     }
     document.addEventListener('DOMContentLoaded', myFunction);
