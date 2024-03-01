e7895504a1ac242c8050b43de9ea83c3e0fad370
rominabrz73 <78692965+rominabrz73@users.noreply.github.com>
2024-03-01 17:39:27 +0000

    week5

--- week5/exercise1/ext1/script.js
+++ week5/exercise1/ext1/script.js
@@ -1,18 +1,20 @@
 function myClickFunction() {
   var element = document.getElementById("circle");
   //element.style.backgroundColor= 'blue';
-  var circleOpacity = parseFloat(element.style.opacity);
+ // var circleOpacity = parseFloat(element.style.opacity);
 
-  element.style.opacity = circleOpacity - 0.1;
+ // element.style.opacity = circleOpacity - 0.1;
+ var positionLeft = element.offsetLeft;
+ element.style.left=positionLeft - 10 +  'px';
 }
 
 function myLoadFunction() {
   var element = document.getElementById("circle");
 
- //element.addEventListener("click", myClickFunction);
+ element.addEventListener("click", myClickFunction);
 
- element.style.opacity = "1";
-  var interval = setInterval( myClickFunction, 1000);
+ //element.style.opacity = "1";
+//var interval = setInterval( myClickFunction, 1000);
 }
 
 document.addEventListener("DOMContentLoaded", myLoadFunction);
