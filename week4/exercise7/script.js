ddb6edd8ac7e93314b3dbc8805cc33e6481fab6f
rominabrz73 <78692965+rominabrz73@users.noreply.github.com>
2024-03-01 16:01:00 +0000

    h

--- week4/exercise7/script.js
+++ week4/exercise7/script.js
@@ -5,22 +5,19 @@
         var element = document.getElementById('inputText');
        var elm  = document.getElementById('showValue');
        elm.firstChild.nodeValue = element.value ;
-       alert(element.value)
-        
-    
-
-
-        
-    
-        
+       //alert(element.value)       
+          
         }
       
         function myLoadFunction(){
 
       
 
-            var pelement = document.getElementById('inputBtn');
-            pelement.addEventListener('click', disMsg);
+           // var pelement = document.getElementById('inputBtn');
+           // pelement.addEventListener('click', disMsg);
+           
+            var pelement = document.getElementById('inputTxt');
+            pelement.addEventListener('keyup', disMsg);
             
         
         }    
