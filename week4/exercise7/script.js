a8e70b679a68c0ba08fec27f782ee18c1e0d0cae
rominabrz73 <78692965+rominabrz73@users.noreply.github.com>
2024-03-08 15:29:46 +0000

    week6

--- week4/exercise7/script.js
+++ week4/exercise7/script.js
@@ -1,27 +1,16 @@
+function disMsg() {
+  var element = document.getElementById("inputText");
+  var elm = document.getElementById("showValue");
+  elm.firstChild.nodeValue = element.value;
+  //alert(element.value)
+}
 
-    function  disMsg(){
+function myLoadFunction() {
+  // var pelement = document.getElementById('inputBtn');
+  // pelement.addEventListener('click', disMsg);
 
-      
-        var element = document.getElementById('inputText');
-       var elm  = document.getElementById('showValue');
-       elm.firstChild.nodeValue = element.value ;
-       //alert(element.value)       
-          
-        }
-      
-        function myLoadFunction(){
+  var pelement = document.getElementById("inputTxt");
+  pelement.addEventListener("keyup", disMsg);
+}
 
-      
-
-           // var pelement = document.getElementById('inputBtn');
-           // pelement.addEventListener('click', disMsg);
-           
-            var pelement = document.getElementById('inputTxt');
-            pelement.addEventListener('keyup', disMsg);
-            
-        
-        }    
-
-   
-    document.addEventListener('DOMContentLoaded', myLoadFunction);
-      
+document.addEventListener("DOMContentLoaded", myLoadFunction);
