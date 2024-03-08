3e4bfbebdc17d023566e547a5696c0a8c07b379f
rominabrz73 <78692965+rominabrz73@users.noreply.github.com>
2024-03-08 15:47:00 +0000

    new

--- week6/exercise1/script.js
+++ week6/exercise1/script.js
@@ -1,8 +1,23 @@
 function myKeyDown(event) {
- console.log(event.keycode);
+  var element = document.getElementById("circle");
+  console.log(event.keyCode);
+if(event.keyCode == 37){
  
+  var positionLeft = element.offsetLeft;
+  element.style.left=positionLeft - 10 +  'px';
+
+}
+if(event.keyCode == 39){
+ 
+  var positionLeft = element.offsetLeft;
+  element.style.left=positionLeft + 10 +  'px';
+
+}
 }
 
 
 
 document.addEventListener('keydown', myKeyDown);
+
+
+
